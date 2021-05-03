19792ef4edc7e9413a9631b26137351f54fecf9a
yourenz <1909154009@qq.com>
2021-05-03 13:42:57 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+
+  plugins: ['prettier', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+}
---
+++ gulpfile.js
@@ -0,0 +1,203 @@
+// https://github.com/godbasin/wxapp-typescript-demo/blob/master/gulpfile.js
+
+const gulp = require('gulp')
+const path = require('path')
+const rename = require('gulp-rename')
+const changed = require('gulp-changed')
+const clear = require('gulp-clean')
+const del = require('del')
+
+const less = require('gulp-less')
+const sass = require('gulp-dart-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+
+
+const ts = require('gulp-typescript')
+const tsProject = ts.createProject('tsconfig.json')
+const sourcemaps = require('gulp-sourcemaps')
+const jsonTransform = require('gulp-json-transform')
+const projectConfig = require('./package.json')
+const alias = require('gulp-ts-alias')
+
+const option = {
+  base: 'src',
+  allowEmpty: true,
+}
+const dist = __dirname + '/dist'
+const copyPath = ['src/**/!(_)*.*', '!src/**/*.less', '!src/**/*.sass', '!src/**/*.scss', '!src/**/*.ts']
+const lessPath = ['src/**/*.less', 'src/app.less']
+const watchLessPath = ['src/**/*.less', 'src/css/**/*.less', 'src/app.less']
+const sassPath = ['src/**/*.sass', 'src/app.sass', 'src/**/*.scss', 'src/app.scss']
+const watchSassPath = ['src/**/*.sass', 'src/app.sass', 'src/**/*.scss', 'src/app.scss']
+const tsPath = ['src/**/*.ts', 'src/app.ts']
+
+gulp.task('clear', () => {
+  return gulp.src(dist, { allowEmpty: true }).pipe(clear())
+})
+
+gulp.task('copy', () => {
+  return gulp.src(copyPath, option).pipe(gulp.dest(dist))
+})
+gulp.task('copyChange', () => {
+  return gulp.src(copyPath, option).pipe(changed(dist)).pipe(gulp.dest(dist))
+})
+
+const dependencies = projectConfig && projectConfig.dependencies // dependencies配置
+const nodeModulesCopyPath = []
+for (let d in dependencies) {
+  nodeModulesCopyPath.push('node_modules/' + d + '/**/*')
+}
+const copyNodeModuleOption = {
+  base: '.',
+  allowEmpty: true,
+}
+
+gulp.task('copyNodeModules', () => {
+  return gulp.src(nodeModulesCopyPath, copyNodeModuleOption).pipe(gulp.dest(dist))
+})
+gulp.task('copyNodeModulesChange', () => {
+  return gulp.src(nodeModulesCopyPath, copyNodeModuleOption).pipe(changed(dist)).pipe(gulp.dest(dist))
+})
+gulp.task('generatePackageJson', () => {
+  return gulp
+    .src('./package.json')
+    .pipe(
+      jsonTransform(function (data, file) {
+        return {
+          dependencies: dependencies,
+        }
+      }),
+    )
+    .pipe(gulp.dest(dist))
+})
+
+gulp.task('less', () => {
+  return gulp
+    .src(lessPath, option)
+    .pipe(
+      less().on('error', function (e) {
+        console.error(e.message)
+        this.emit('end')
+      }),
+    )
+    .pipe(postcss([autoprefixer]))
+    .pipe(
+      rename(function (path) {
+        path.extname = '.wxss'
+      }),
+    )
+    .pipe(gulp.dest(dist))
+})
+gulp.task('lessChange', () => {
+  return gulp
+    .src(lessPath, option)
+    .pipe(changed(dist))
+    .pipe(
+      less().on('error', function (e) {
+        console.error(e.message)
+        this.emit('end')
+      }),
+    )
+    .pipe(postcss([autoprefixer]))
+    .pipe(
+      rename(function (path) {
+        path.extname = '.wxss'
+      }),
+    )
+    .pipe(gulp.dest(dist))
+})
+
+gulp.task('sass', () => {
+  return gulp
+    .src(sassPath, option)
+    .pipe(
+      sass().on('error', function (e) {
+        console.error(e.message)
+        this.emit('end')
+      }),
+    )
+    .pipe(postcss([autoprefixer]))
+    .pipe(
+      rename(function (path) {
+        path.extname = '.wxss'
+      }),
+    )
+    .pipe(gulp.dest(dist))
+})
+gulp.task('sassChange', () => {
+  return gulp
+    .src(sassPath, option)
+    .pipe(changed(dist))
+    .pipe(
+      sass().on('error', function (e) {
+        console.error(e.message)
+        this.emit('end')
+      }),
+    )
+    .pipe(postcss([autoprefixer]))
+    .pipe(
+      rename(function (path) {
+        path.extname = '.wxss'
+      }),
+    )
+    .pipe(gulp.dest(dist))
+})
+
+gulp.task('tsCompile', function () {
+  return tsProject
+    .src()
+    .pipe(alias({ configuration: tsProject.config }))
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .js.pipe(
+      sourcemaps.write({
+        sourceRoot: (file) => path.relative(path.join(file.cwd, file.path), file.base),
+      }),
+    )
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch(tsPath, gulp.series('tsCompile'))
+  const watcher = gulp.watch(copyPath, gulp.series('copyChange'))
+  gulp.watch(nodeModulesCopyPath, gulp.series('copyNodeModulesChange'))
+  gulp.watch(watchLessPath, gulp.series('lessChange')) //Change
+  gulp.watch(watchSassPath, gulp.series('sassChange')) //Change
+  watcher.on('change', function (event) {
+    if (event.type === 'deleted') {
+      const filepath = event.path
+      const filePathFromSrc = path.relative(path.resolve('src'), filepath)
+      // Concatenating the 'build' absolute path used by gulp.dest in the scripts task
+      const destFilePath = path.resolve('dist', filePathFromSrc)
+      // console.log({filepath, filePathFromSrc, destFilePath})
+      del.sync(destFilePath)
+    }
+  })
+})
+
+gulp.task(
+  'default',
+  gulp.series(
+    // sync
+    gulp.parallel('copy', 'copyNodeModules', 'generatePackageJson', 'less', 'sass', 'tsCompile'),
+    'watch',
+  ),
+)
+
+gulp.task(
+  'build',
+  gulp.series(
+    // sync
+    'clear',
+    gulp.parallel(
+      // async
+      'copy',
+      'copyNodeModules',
+      'generatePackageJson',
+      'less',
+      'sass',
+      'tsCompile',
+    ),
+  ),
+)
