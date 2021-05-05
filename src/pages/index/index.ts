534cbeb4b7e504b8dae57e5e919eebcf625a5d36
yourenz <1909154009@qq.com>
2021-05-05 22:14:44 +0800

    add watch and computed

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,3 +1,22 @@
+import * as computed from 'miniprogram-computed'
+
 Component({
-  data: {},
+  data: {
+    a: 1,
+    b: 2,
+    sum: 2,
+  },
+  behaviors: [computed.behavior],
+  watch: {
+    'a, b': function (a, b) {
+      this.setData({
+        sum: a + b,
+      })
+    },
+  },
+  computed: {
+    sumc: function (data) {
+      return data.a + data.b
+    },
+  },
 })
